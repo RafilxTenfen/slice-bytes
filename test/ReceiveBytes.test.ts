f89d8832aba1953e8f63a9b7d4611b57744c9eb0
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-09-01 09:33:55 -0300

    add tests for the receive bytes

--- test/ReceiveBytes.test.ts
+++ test/ReceiveBytes.test.ts
@@ -1,14 +1,30 @@
 import { ethers } from "hardhat";
 import { Signer } from "ethers";
+import { expect } from 'chai'
+import Web3 from 'web3';
 
-describe("Token", function () {
+describe("Receive Bytes tests", function () {
   let accounts: Signer[];
+  let deployer: Signer;
+  let other: Signer;
 
   beforeEach(async function () {
-    accounts = await ethers.getSigners();
+    [deployer, ...accounts] = await ethers.getSigners();
   });
 
-  it("should do something right", async function () {
-    // Do something with the accounts
+  it("should deploy with deployer", async function () {
+    const receiveBytesFactory = await ethers.getContractFactory("ReceiveBytes", other);
+
+    const receiveBytes = await receiveBytesFactory.deploy();
+    expect(await receiveBytes.owner()).to.equal(await deployer.getAddress());
+  });
+
+  it("should receive data", async function () {
+    const receiveBytesFactory = await ethers.getContractFactory("ReceiveBytes");
+
+    const receiveBytes = await receiveBytesFactory.deploy();
+    const dataBytes = Web3.utils.encodePacked('anything', 'it can be multiples stuffs');
+    await receiveBytes.receiveData(dataBytes);
+    expect(await receiveBytes.data()).to.equal(dataBytes);
   });
 });
